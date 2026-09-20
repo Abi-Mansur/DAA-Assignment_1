f8854564e26757cf25e40b454abad5bab00e219e
Mansur <251868@astanait.edu.kz>
2026-09-20 19:43:57 +0500

    test commit

---
+++ MergeSort.java
@@ -0,0 +1,16 @@
+public class MergeSort {
+
+    private static int CutOff = 15;
+
+    public void sort (int[] a, Metrics metrics){
+
+        if (a == null and or a.length <= 1){
+            return;
+        }
+
+        int [temp]
+
+    }
+
+
+}
