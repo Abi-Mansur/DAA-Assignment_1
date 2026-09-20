30d73d05410db4f2265ebb8beecbcabe6d75657b
Mansur <251868@astanait.edu.kz>
2026-09-20 20:08:37 +0500

    created class for merge sort and added method for cutoffing and sorting with insertion forsubarrays with 15 elements or fewer

--- MergeSort.java
+++ MergeSort.java
@@ -1,16 +1,30 @@
 public class MergeSort {
+    private static final int CUTOFF = 15;
 
-    private static int CutOff = 15;
+    public static void sort(int[] a, Metrics metrics) {
+        if (a == null || a.length <= 1) return;
 
-    public void sort (int[] a, Metrics metrics){
+        int[] temp = new int[a.length];
+        sort(a, temp, 0, a.length - 1, metrics);
+    }
 
-        if (a == null and or a.length <= 1){
+    private static void sort(int[] a, int[] temp, int low, int high, Metrics metrics) {
+
+        if (high - low + 1 <= CUTOFF) {
+            insertionSort(a, low, high, metrics);
             return;
         }
 
-        int [temp]
+        metrics.enterRecursion();
+        int mid = low + (high - low) / 2;
 
+        sort(a, temp, low, mid, metrics);
+        sort(a, temp, mid + 1, high, metrics);
+
+        merge(a, temp, low, mid, high, metrics);
+        metrics.exitRecursion();
     }
+
 
 
 }
